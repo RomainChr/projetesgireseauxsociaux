b42204939666dc3e3a544839e5744c68d0db50f0
RomainChr <Romain@MacBook-Pro-de-Romain.local>
2015-04-07 15:50:12 +0200

    4

--- index.php
+++ index.php
@@ -19,23 +19,25 @@
     <title>Document</title>
 </head>
 <body>
+<!--
    <script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '451317511698280',
-      xfbml      : true,
-      version    : 'v2.3'
-    });
-  };
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '451317511698280',
+          xfbml      : true,
+          version    : 'v2.3'
+        });
+      };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+    </script>
+-->
     <h1>Mon App</h1>
     <div
         class="fb-like"
