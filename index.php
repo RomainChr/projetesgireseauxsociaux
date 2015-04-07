3d74cdc2beaf2d1b7ebb083ffc19f8b7c70dcbf4
RomainChr <Romain@MacBook-Pro-de-Romain.local>
2015-04-07 15:53:06 +0200

    5

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<?php
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
-    session_start();
 
-    require "facebook-php-sdk-v4-4.0-dev/autoload.php"
-
-    const APPID = "451317511698280";
-    const APPSECRET = "defa21c582018c4dc20e7a955b8a2e5c";
-    
-    FacebookSession::setDefaultApplication(APPID, APPSECRET);
-?>
 
 
 <!DOCTYPE html>
