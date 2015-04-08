a1eda126ca3a221afc7f9d1611f8171cf966c1e7
RomainChr <Romain@macbook-pro-de-romain.home>
2015-04-08 16:31:54 +0200

    ok

--- index.php
+++ index.php
@@ -1,39 +1,35 @@
 <?php
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
+    
     session_start();
 
     require "facebook-php-sdk-v4-4.0-dev/autoload.php";
-
+    
+    use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+	use Facebook\GraphUser;
+    
     const APPID = "451317511698280";
     const APPSECRET = "defa21c582018c4dc20e7a955b8a2e5c";
     
     FacebookSession::setDefaultApplication(APPID, APPSECRET);
+
     $helper = new FacebookRedirectLoginHelper("https://projetesgireseauxsociaux.herokuapp.com/");
-    $helper = new FacebookRedirectLoginHelper($url_redirect);
+    	
+	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
+	// alors je veux créer mon utilisateur à partir de cette session
+	if( isset($_SESSION) && isset($_SESSION['fb_token']) )
+	{
+		$session = new FacebookSession($_SESSION['fb_token']);
+	}
+	//Sinon j'affiche le lien de connection
+	else
+	{
+		$session = $helper->getSessionFromRedirect();
+	}
 
-    //$loginUrl = $helper->getLoginUrl();
-
-    if(isset($_SESSION) && isset($_SESSION['fb_taken']))
-    {
-        $session = new Facebook($_SESSION['fb_token']);
-    }
-    else
-    {
-        $session = $helper->getSessionFromRedirect();
-    }
-
-
-//    try {
-//        $session = $helper->getSessionFromRedirect();
-//    } catch(FacebookRequestException $ex) {
-//        // When Facebook returns an error
-//    } catch(\Exception $ex) {
-//        // When validation fails or other local issues
-//    }
-//    if ($session) {
-//        // Logged in
-//    }
 ?>
 
 <!DOCTYPE html>
@@ -43,11 +39,10 @@
     <title>Document</title>
 </head>
 <body>
-
    <script>
       window.fbAsyncInit = function() {
         FB.init({
-          appId      : '451317511698280',
+          appId      : '<?php echo APPID; ?>',
           xfbml      : true,
           version    : 'v2.3'
         });
@@ -61,7 +56,9 @@
          fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));
     </script>
+    
     <h1>Mon App</h1>
+    
     <div
         class="fb-like"
         data-share="true"
@@ -69,23 +66,22 @@
         data-show-faces="true">
     </div>
     <?php
-        //Si variable de sessions existent et que $_SESSION ['fb_taken'] existe
+
         if($session)
-        {
-            $_SESSION['fb_token'] = (string) $session->getAccessToken();
-            $user = new FacebookRequest($session,"GET","/me")->getGraphObject(Graph);
-            $request_user_execute = $request_user->execute();
-            $user = $request_user_executed->getGraph;
-            
-            //var_dump($session['fb_taken']);
-            echo "Bonjour ".$user->getName();
-        }
-        //Sinon j'affiche le lien de connection
-        else
-        {
-            $loginUrl = $helper->getLoginUrl();
-            echo "<a href='".$loginUrl."'>Se connecter</a>";
-        }
+			{
+				$_SESSION['fb_token'] = (string) $session->getAccessToken();
+
+				$request_user = new FacebookRequest( $session,"GET","/me");
+				$request_user_executed = $request_user->execute();
+				$user = $request_user_executed->getGraphObject(GraphUser::className());
+
+				echo "Bonjour ".$user->getName();
+
+			}else{
+				$loginUrl = $helper->getLoginUrl();
+				echo "<a href='".$loginUrl."'>Se connecter</a>";
+			}
+
     ?>
 </body>
 </html>
